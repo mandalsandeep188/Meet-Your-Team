1693191599c7386ae34fae1504a2aea341bfea24
Sandeep <mandalsandeep188@gmail.com>
2021-06-14 00:37:33 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -21,15 +21,15 @@ app.use(express.json());
 // Routes
 app.use(require("./routes/auth"));
 
-//Production setup
+// Production setup
 
-// if(process.env.NODE_ENV=="production"){
-//   app.use(express.static('client/build'))
-//   const path = require('path')
-//   app.get("*",(req,res)=>{
-//       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//   })
-// }
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Listening server on the PORT
 server.listen(PORT, () => {
