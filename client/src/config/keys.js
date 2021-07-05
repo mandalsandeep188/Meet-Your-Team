81c8cb6974c7349b375d1fee52a21650d7c3310f
Sandeep <mandalsandeep188@gmail.com>
2021-07-05 18:05:35 +0530

    config update

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,11 +1,10 @@
 // config keys based on production or dev environment
-import devConfig from "./dev";
-import prodConfig from "./prod";
 let config;
 if (process.env.NODE_ENV === "production") {
+  const prodConfig = require("./prod");
   config = prodConfig;
 } else {
+  const devConfig = require("./dev");
   config = devConfig;
 }
-console.log(config);
 export default config;
