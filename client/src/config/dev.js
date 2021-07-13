733bbfecf10feaa595330abbf26ea13dbc544289
Sandeep <mandalsandeep188@gmail.com>
2021-07-13 11:23:49 +0530

    Final commit

---
+++ client/src/config/dev.js
@@ -0,0 +1,6 @@
+// dev client config variables
+export default {
+  PEER_PORT: 5000,
+  SERVER: "http://localhost:5000",
+  CLIENT: "http://localhost:3000",
+};
---
+++ config/dev.js
@@ -0,0 +1,6 @@
+// Keys for dev
+module.exports = {
+  MONGO_URL:
+    "mongodb+srv://Sandeep188:8447291272@cluster0.rwkkt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  JWT_SECRET: "uishdfiuh",
+};
